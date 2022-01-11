b54c1b19747ff17a49b836d656754f94cca20079
Carolinayuqui28 <carolinayuqui28@outlook.es>
2022-01-11 01:17:40 -0500

    módulo de contactos
    Implementación de una barra de navegación interna de contactos, empresas y prospectos. Se agregó el botón de añadir nuevo, falta implementar funcionalidad.

--- nav_bar/lib/buscarpagina.dart
+++ nav_bar/lib/buscarpagina.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart'; // importa del main
-
-class BuscarPagina extends StatelessWidget {
+class BuscarPagina extends StatefulWidget {
   const BuscarPagina({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
-
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<BuscarPagina> createState() => _MyStatefulWidgetState();
 }
 
 /// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
-class _MyStatefulWidgetState extends State<MyStatefulWidget>
+class _MyStatefulWidgetState extends State<BuscarPagina>
     with TickerProviderStateMixin {
   late TabController _tabController;
 
@@ -35,30 +20,51 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal.shade50, // fonde de la otra pagina
+      backgroundColor: Colors.teal.shade50, // fondo de la otra pagina
       appBar: AppBar(
-        backgroundColor: Colors.teal.shade400, // fonde de la otra pagina
+        backgroundColor: Colors.teal.shade400, // fondo de la AppBar
         title: const Text('Contactos'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('buscar')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.filter_list),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Filtrar')));
+            },
+          ),
+        ],
+        
         bottom: TabBar(
           controller: _tabController,
           tabs: const <Widget>[
             Tab(
-              child: Text("Contactos"),
+              child: Text("Contactos",style: TextStyle(fontSize: 18),),
             ),
             Tab(
-               child: Text("Empresas"),
+               child: Text("Empresas",style: TextStyle(fontSize: 18),),
             ),
             Tab(
-               child: Text("Prospectos"),
+               child: Text("Prospectos",style: TextStyle(fontSize: 18),),
             ),
           ],
         ),
       ),
       body: TabBarView(
         controller: _tabController,
+
         children: const <Widget>[
           Center(
             child: Text("personas individuales"),
+            
           ),
           Center(
             child: Text("empresas "),
@@ -67,7 +73,18 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget>
             child: Text("posibles clientes"),
           ),
         ],
+        
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal.shade400,
+        onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('agregar contactos')));
+            },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
     );
   }
 }
